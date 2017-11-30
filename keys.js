e9a363e9dc59769240a3eb2ffc234573e68b0d0e
kferny1 <30305285+kferny1@users.noreply.github.com>
2017-11-29 22:33:23 -0700

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,18 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '	09LoXyBJAzh9XQOB9swP03jQp',
+  consumer_secret: '	Py3E2rAOSEvl6Or5X6lXVucTBNkvFqAH3DtNofb3koEIkK1AVx',
+  access_token_key: '935928117062877184-W55fa4lBLKXAwbm9h8Y4OYbbefaaAM3',
+  access_token_secret: 'hskFDPfxtFbaOMVys8gGmh2GdaUVIsPvH7sbvyXENgLH0'
+}
+
+var spotifyKeys = {
+	client_ID: '44b807aa139548f2a6aa397b8db4fda5',
+  	client_secret: '6608cebfb00c41548e1d2df54c274582'
+
+}
+
+var omdbApiKey = {
+	API_key:"2b2faf8c"
+}
